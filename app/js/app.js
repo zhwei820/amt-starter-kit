465d324ff466a7fd8608bea78afb82fb6e05b966
minwe <minwe@yunshipei.com>
2016-06-08 10:31:04 +0800

    add React Router `withRouter` HoC doc links

--- app/js/app.js
+++ app/js/app.js
@@ -49,6 +49,7 @@ class App extends React.Component {
             icon="info"
             title="关于"
             badge="404"
+            // @see https://github.com/reactjs/react-router/blob/0616f6e14337f68d3ce9f758aa73f83a255d6db3/docs/API.md#isactivepathorloc-indexonly
             selected={router.isActive('/about', true)}
             to="/about"
           />
@@ -62,6 +63,8 @@ class App extends React.Component {
 import Index from './pages/Index';
 import Page from './pages/Page';
 
+// withRouter HoC
+// @see https://github.com/reactjs/react-router/blob/0616f6e14337f68d3ce9f758aa73f83a255d6db3/upgrade-guides/v2.4.0.md#v240-upgrade-guide
 const routes = (
   <Router history={hashHistory}>
     <Route path="/" component={withRouter(App)}>
